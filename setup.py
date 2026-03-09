02e0a78b0e3db542ecc45b347ddff0b245e90f69
Arganex-Emad <80041099+EmadYaY@users.noreply.github.com>
2026-03-09 13:21:46 +0330

    Update setup.py
    Signed-off-by: Arganex-Emad <80041099+EmadYaY@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,10 +1,36 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import platform
+
+# pywin32 only on Windows
+win_deps = ['pywin32'] if platform.system() == 'Windows' else []
 
 setup(
     name='phishiurl',
-    version='1.2.8',
-    packages=['phishiurl'],
-    install_requires=['click', 'rich', 'requests', 'pyngrok', 'python-whois', 'qrcode', 'beautifulsoup4', 'lxml', 'pywin32', 'selenium'],
-    entry_points={'console_scripts': ['phishiurl=phishiurl.cli:cli']},
-    package_data={'phishiurl': ['data/*.json', 'templates/*']},
+    version='1.3.0',
+    description='Phishing Detection and Simulation Tool',
+    author='Emad',
+    url='https://github.com/EmadYaY/PhishiUrl',
+    packages=find_packages(),
+    python_requires='>=3.7',
+    install_requires=[
+        'click',
+        'rich',
+        'requests',
+        'pyngrok',
+        'python-whois',
+        'qrcode',
+        'beautifulsoup4',
+        'lxml',
+        'selenium',
+        'webdriver-manager',
+    ] + win_deps,
+    extras_require={
+        'dev': ['pytest'],
+    },
+    entry_points={
+        'console_scripts': ['phishiurl=phishiurl.cli:cli'],
+    },
+    package_data={
+        'phishiurl': ['data/*.json', 'templates/*'],
+    },
 )
